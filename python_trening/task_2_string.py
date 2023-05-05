6d5894eb3ea086e6578e6fb4952ff8bf3c8f6aff
zhendrikpalama <zhenyapalamarchuk03@gmail.com>
2023-05-05 17:18:25 +0300

    Добавление файлов в проект

---
+++ python_trening/task_2_string.py
@@ -0,0 +1,14 @@
+s = "Это строка"
+print(s + '\n')
+g = '''Многострочная
+строка'''
+print(g + '\n')
+print('Строки ' + 'можно ' + 'складывать ')
+
+print('Auto' * 3)
+
+print(len('test'))
+
+a = 'Auto'
+print(a[0])
+print(a[-2])
---
+++ python_trening/исла.py
@@ -0,0 +1,11 @@
+print(6 + 2)
+print(6 - 2)
+print(6 * 2)
+print(6 / 2)
+
+print(7 / 2)
+print(7 // 2)
+
+print(7 % 2)
+
+print(6 ** 2)
